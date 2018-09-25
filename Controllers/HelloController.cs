dbbdfb6d8504c18ea94270dbcdd392bf138cfd1c
iFew <chitpong@gmail.com>
2018-09-25 15:35:01 +0700

    Removed https redirect

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hello.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Hello World";
+        }
+
+        // GET api/values/5
+        [HttpGet("{name}")]
+        public ActionResult<string> Get(string name)
+        {
+            return name;
+        }
+    }
+}
